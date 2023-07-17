e551c1b4637ed73975e820d9e1c9e05da214985b
BerlofaBosi <68667433+BerlofaBosi@users.noreply.github.com>
2023-07-17 01:26:20 -0300

    Testes com loops em python e cpp

---
+++ TESTES/test.py
@@ -0,0 +1,12 @@
+
+c = 0
+
+while c != 1000000:
+    c += 1
+    print(c)
+
+
+for i in range(0, 1000000):
+    i += 1
+    print(i)
+    
---
+++ TESTES/teste.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main() {
+
+    int c = 0;
+
+    while (c != 1000) {
+
+        c += 1;
+        cout << c << endl;
+
+    }
+
+    return 0;
+}
+*/
+
+int main() {
+
+    for (int c = 0; c <= 1000; c++) {
+
+        cout << "TESTE" << " " << c << endl;
+
+    }
+
+    return 0;
+}
